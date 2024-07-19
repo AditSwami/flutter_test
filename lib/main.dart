292e54ee93c8b0f5ae84a443c040c3d6185063fe
AditSwami <swamiaditswami@gmail.com>
2024-07-19 17:25:56 -0500

    adding flutter files

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.black),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Wordpair Generator')),
+        //body: const Center(child: Text(wordPair.asPascalCase)) 
+    ));
+  }
+}
