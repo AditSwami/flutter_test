a0784848317ac57bfe20675eadf3b2dff5bd565b
AditSwami <swamiaditswami@gmail.com>
2024-07-22 13:39:12 -0500

    added new widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,40 @@
-import 'package:english_words/english_words.dart';
+//import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+//import 'package:flutter/widgets.dart';
 
-void main() => runApp(MyApp());
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(primarySwatch: Colors.blueGrey)
+  ));
+}
 
-class MyApp extends StatelessWidget{
+class HomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    final wordPair = WordPair.random();
-    return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.black),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Wordpair Generator')),
-        //body: const Center(child: Text(wordPair.asPascalCase)) 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Awsome App"),
+        backgroundColor: Colors.red
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Text("Hello World"),
+          //color: Colors.blueAccent,
+          width: 100,
+          height: 100,
+          alignment: Alignment.topLeft,
+          decoration: BoxDecoration(
+            color: Colors.blueGrey,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 10,
+              )]),
+          clipBehavior: Clip.antiAlias,
+          ),
     ));
   }
 }
